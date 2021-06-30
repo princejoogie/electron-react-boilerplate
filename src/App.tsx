f2691df67dab07824ebfa580e161c49fe99d9ca6
princejoogie <princejoogie@gmail.com>
2021-07-01 04:18:14 +0800

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <p className="text-red-500 text-2xl">Hello World</p>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-800">
+      <div className="flex space-x-2 items-center">
+        <h1 className="text-white text-2xl">Hello World</h1>
+        <div className="animate-spin text-3xl">🌎</div>
+      </div>
     </div>
   );
 };
